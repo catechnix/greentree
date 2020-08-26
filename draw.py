1bfa832f2a3bb9f4d1c1b07caeea383b5d578023
catechnix <catechnix@yahoo.com>
2020-08-26 15:54:51 -0700

    Add files via upload

---
+++ draw.py
@@ -0,0 +1,18 @@
+from swampy.TurtleWorld import *
+
+world=TurtleWorld()
+t=Turtle()
+
+def draw(t, length, n): 
+    if n == 0: 
+        return 
+    angle = 50 
+    fd(t, length*n) 
+    lt(t, angle) 
+    draw(t, length, n-1) 
+    rt(t, 2*angle) 
+    draw(t, length, n-1) 
+    lt(t, angle) 
+    bk(t, length*n)
+
+draw(t, 25, 4)
