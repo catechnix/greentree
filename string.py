6a46d15955a1233e1d421ca336e3956f24d516b1
catechnix <catechnix@yahoo.com>
2020-09-13 13:11:27 -0700

    Add files via upload

---
+++ string.py
@@ -0,0 +1,9 @@
+word ="   banana"
+count_num=word.count("a")
+print(count_num)
+
+strip_letter=word.strip(" a")
+print (strip_letter)
+
+replace_letter=word.replace("a","t",2)
+print (replace_letter)
